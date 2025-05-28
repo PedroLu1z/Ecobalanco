b80dc5702f2a8fafc91eab3b7182034912971330
Pedro.Santana <Pedro.Santana@tiberio.com.br>
2025-05-28 12:06:44 -0300

    Adicionado Rotas

--- ecobalanco/src/app/app.routes.ts
+++ ecobalanco/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CalculadoraComponent } from './components/calculadora/calculadora.component';
+import { PraticasSustentaveisComponent } from './components/praticas-sustentaveis/praticas-sustentaveis.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent
+    },
+    {
+        path: "/calculadora",
+        component: CalculadoraComponent
+    },
+    {
+        path: "/praticasSustentaveis",
+        component: PraticasSustentaveisComponent
+    }
+];
